a0828b178239917a9a65747b9a350005488c18db
hero13th <210customshirts@gmail.com>
2026-06-30 15:06:29 -0500

    update app frontend

--- script.js
+++ script.js
@@ -1,28 +1,110 @@
-import express from "express";
-import cors from "cors";
+const API = "https://aivideoapp-wb8p.onrender.com";
 
-const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("AI SERVER RUNNING ✔");
+async function generateImage(){
+
+const prompt = document.getElementById("prompt").value.trim();
+
+
+if(!prompt){
+
+alert("Enter prompt");
+return;
+
+}
+
+
+console.log("Sending image:", prompt);
+
+
+const response = await fetch(`${API}/image`,{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+prompt:prompt
+})
+
 });
 
-app.post("/image", (req, res) => {
-  res.json({
-    result: "https://via.placeholder.com/512"
-  });
+
+const data = await response.json();
+
+
+console.log(data);
+
+
+if(data.result){
+
+document.getElementById("image").src = data.result;
+
+}else{
+
+alert("No image returned");
+
+}
+
+
+}
+
+
+
+
+async function generateVideo(){
+
+
+const prompt = document.getElementById("prompt").value.trim();
+
+
+if(!prompt){
+
+alert("Enter prompt");
+return;
+
+}
+
+
+console.log("Sending video:", prompt);
+
+
+
+const response = await fetch(`${API}/generate`,{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({
+prompt:prompt
+})
+
 });
 
-app.post("/generate", (req, res) => {
-  res.json({
-    result: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4"
-  });
-});
 
-const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on", PORT);
-});
+const data = await response.json();
+
+
+console.log(data);
+
+
+
+if(data.result){
+
+document.getElementById("video").src = data.result;
+
+
+}else{
+
+alert("No video returned");
+
+}
+
+
+}
